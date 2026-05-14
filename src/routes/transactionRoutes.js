b04d821ad8c8ed4c7d0f26573b37d0a68f0923a2
Aditya Gaikwad <adityabgaikwad0012@gmail.com>
2026-05-15 01:23:45 +0530

    update backend

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,1 +1,17 @@
+const router = require("express").Router();
+const {
+	createTransaction,
+	getTransactionHistory
+} = require("../controllers/transactionController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.use(authMiddleware);
+
+// Transaction creation
+router.post("/", createTransaction);
+
+// Transaction history
+router.get("/history", getTransactionHistory);
+
+module.exports = router;
 
